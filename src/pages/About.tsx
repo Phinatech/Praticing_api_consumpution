b262cbc81dcc896fb1c69064ef6418314e351589
Phinatech <ugbojudithchinemerem@gmail.com>
2023-04-04 16:10:15 +0100

    starting 2nd praatice

--- src/pages/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <h2>Welcome to my About Page💋👏👏👏👏🙌🙌🙌</h2>;
-};
-
-export default About;
--- src/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Home } from "./About";
-export { default as About } from "./About";
