53614ea4867705ef9d9f1ec18388a1c87a211bfe
Phinatech <ugbojudithchinemerem@gmail.com>
2023-04-04 13:47:21 +0100

    updating

---
+++ src/components/layouts/homelayout/Homelayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homelayout = () => {
+  return <div>Homelayout</div>;
+};
+
+export default Homelayout;
---
+++ src/components/layouts/homelayout/index.ts
@@ -0,0 +1,1 @@
+export { default as homelayout } from "./Homelayout";
